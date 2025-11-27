53e876a9e43777109d6d249a500e69311302083e
vathana <siengvathana1@gmail.com>
2025-11-27 20:22:24 +0700

    d

--- routes/api.php
+++ routes/api.php
@@ -48,7 +48,7 @@ Route::post('/collector/register', [CollectorAuthController::class, 'register'])
 Route::post('/collector/login', [CollectorAuthController::class, 'login']); 
 Route::get('/collector/index', [CollectorAuthController::class, 'index']); 
 Route::post('/collector/save', [CustomerController::class, 'store']);
-Route::get('/collector/user', [CustomerController::class, 'user']);
+Route::get('/collector/user', [CollectorAuthController::class, 'user']);
 
 // Route::middleware(['jwt.cookie', 'auth:api'])->group(function () {
 //     Route::get('/collector/user', [CollectorAuthController::class, 'user']);
