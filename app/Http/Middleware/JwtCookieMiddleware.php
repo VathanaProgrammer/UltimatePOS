1380e2c4aaa4a8fb00b365e8f1f2ad3eab904316
vathana <siengvathana1@gmail.com>
2025-11-28 10:16:21 +0700

    u

--- app/Http/Middleware/JwtCookieMiddleware.php
+++ app/Http/Middleware/JwtCookieMiddleware.php
@@ -12,20 +12,14 @@ class JwtCookieMiddleware
     public function handle($request, Closure $next)
     {
         try {
-            // 1. Check if cookie exists
             if ($token = $request->cookie('c_token')) {
-                JWTAuth::setToken($token);
-
-                // 2. Authenticate user
-                $user = JWTAuth::parseToken()->authenticate();
-
-                // 3. Attach user to request
+                $user = JWTAuth::setToken($token)->authenticate(); // parse & authenticate
                 $request->merge(['user' => $user]);
             } else {
-                return response()->json(['message' => 'Unauthenticated.'], Response::HTTP_UNAUTHORIZED);
+                return response()->json(['message' => 'Unauthenticated.'], 401);
             }
         } catch (JWTException $e) {
-            return response()->json(['message' => 'Unauthenticated.'], Response::HTTP_UNAUTHORIZED);
+            return response()->json(['message' => 'Unauthenticated.'], 401);
         }
 
         return $next($request);
