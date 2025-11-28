6ace44ca2fd384a6573439339c16769445d0c6cb
vathana <siengvathana1@gmail.com>
2025-11-28 07:30:20 +0700

    f

--- app/Http/Controllers/Api/CollectorAuthController.php
+++ app/Http/Controllers/Api/CollectorAuthController.php
@@ -56,7 +56,7 @@ class CollectorAuthController extends Controller
                 $token,
                 60*24*7,       // 1 week
                 '/',            // path
-                $this->cookieDomain,  // domain
+                '.syspro.asia',  // domain
                 true,           // secure
                 true,           // httpOnly
                 false,          // raw
@@ -113,7 +113,7 @@ class CollectorAuthController extends Controller
                 $token,
                 60*24*7,
                 '/',
-                $this->cookieDomain,
+                '.syspro.asia',
                 true,
                 true,
                 false,
@@ -162,7 +162,7 @@ class CollectorAuthController extends Controller
                 '',
                 -1,
                 '/',
-                $this->cookieDomain,
+                '.syspro.asia',
                 true,
                 true,
                 false,
