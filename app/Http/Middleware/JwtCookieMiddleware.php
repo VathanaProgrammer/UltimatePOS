0ee3dbb4e8b8ffe034e3609c39db666b4434be95
vathana <siengvathana1@gmail.com>
2025-11-28 10:06:00 +0700

    g

--- app/Http/Middleware/JwtCookieMiddleware.php
+++ app/Http/Middleware/JwtCookieMiddleware.php
@@ -4,14 +4,30 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
 
 class JwtCookieMiddleware
 {
     public function handle($request, Closure $next)
     {
-        if ($token = $request->cookie('c_token')) {
-            JWTAuth::setToken($token);
+        try {
+            // 1. Check if cookie exists
+            if ($token = $request->cookie('c_token')) {
+                JWTAuth::setToken($token);
+
+                // 2. Authenticate user
+                $user = JWTAuth::parseToken()->authenticate();
+
+                // 3. Attach user to request
+                $request->merge(['user' => $user]);
+            } else {
+                return response()->json(['message' => 'Unauthenticated.'], Response::HTTP_UNAUTHORIZED);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['message' => 'Unauthenticated.'], Response::HTTP_UNAUTHORIZED);
         }
+
         return $next($request);
     }
 }
