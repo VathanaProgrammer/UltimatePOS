983ed000047c7a135f60d70843ec23b7b6ebe04f
vathana <siengvathana1@gmail.com>
2025-12-06 18:01:13 +0700

    g\

--- resources/views/sale_pos/receipts/delivery_label.blade.php
+++ resources/views/sale_pos/receipts/delivery_label.blade.php
@@ -52,7 +52,7 @@
             Date: {{ \Carbon\Carbon::now()->format('d/m/Y H:iA') }}
         </div>
         <div class="qr-code" style="text-align:center; margin-top:20px;">
-             {!! $qrcode !!}
+            <img src="data:image/png;base64,{{ $qrcode }}" alt="QR Code" />
         </div>
 
         <div class="customer-info">
