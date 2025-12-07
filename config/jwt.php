18aad544b43dda782135abff1b916995d87c357d
vathana <siengvathana1@gmail.com>
2025-12-08 01:41:44 +0700

    f

--- config/jwt.php
+++ config/jwt.php
@@ -27,6 +27,8 @@ return [
 
     'secret' => env('JWT_SECRET'),
 
+    'ttl' => env('JWT_TTL', 60),
+
     /*
     |--------------------------------------------------------------------------
     | JWT Authentication Keys
