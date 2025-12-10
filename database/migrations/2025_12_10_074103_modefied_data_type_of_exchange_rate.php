31bb69fe166a5c933e9feb846091ecac0e9e10d3
vathana <siengvathana1@gmail.com>
2025-12-10 09:17:21 +0700

    g

--- database/migrations/2025_12_10_074103_modefied_data_type_of_exchange_rate.php
+++ database/migrations/2025_12_10_074103_modefied_data_type_of_exchange_rate.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     /**
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('currencies', function (Blueprint $table) {
             //
-            $table->integer('exchange_rate')->nullable()->default(0)->change();
+            DB::statement('ALTER TABLE currencies MODIFY exchange_rate INT 0;');
         });
     }
 
