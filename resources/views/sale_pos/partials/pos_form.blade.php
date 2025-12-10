ed3b8a9f706cac15b3829ee56605acc2bd07eb49
vathana <siengvathana1@gmail.com>
2025-12-10 11:48:20 +0700

    g

--- resources/views/sale_pos/partials/pos_form.blade.php
+++ resources/views/sale_pos/partials/pos_form.blade.php
@@ -65,7 +65,7 @@
             </div>
         </div>
     </div>
-    <div class="col-12 col-sm-6 col-md-4 d-flex justify-content-end mb-3">
+    {{-- <div class="col-12 col-sm-6 col-md-4 d-flex justify-content-end mb-3">
         {!! Form::label('exchange_rate', 'Exchange Rate', ['class' => 'form-label mr-2']) !!}
         {!! Form::number('exchange_rate', $exchange_rate ?? 0.00, [
             'class' => 'form-control text-right',
@@ -73,7 +73,7 @@
             'placeholder' => '0.00',
 			'id' => 'exchange_rate',
         ]) !!}
-    </div>
+    </div> --}}
 
 </div>
 <div class="row">
