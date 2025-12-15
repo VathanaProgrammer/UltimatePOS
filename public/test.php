22381d0553e68f8bace41b863cbd87183a3228b8
vathana <siengvathana1@gmail.com>
2025-12-15 11:20:16 +0700

    g

--- public/test.php
+++ public/test.php
@@ -4,7 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/vendor/autoload.php'; // Make sure Composer autoload is included
+require __DIR__ . '/../vendor/autoload.php';
+ // Make sure Composer autoload is included
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 // Example data
