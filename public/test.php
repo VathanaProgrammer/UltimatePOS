89e673bf3cd7e07b2b9be94708ceba901652cd7f
vathana <siengvathana1@gmail.com>
2025-12-15 11:28:52 +0700

    g

--- public/test.php
+++ public/test.php
@@ -6,7 +6,7 @@ $white = imagecolorallocate($img, 255, 255, 255);
 $black = imagecolorallocate($img, 0, 0, 0);
 imagefill($img, 0, 0, $white);
 
-$font = __DIR__ . '/fonts/khmer/NotoSansKhmer-Regular.ttf';
+$font = __DIR__ . '/fonts/khmer/Battambang-Regular.ttf';
 $text = 'សួស្តី';
 
 if (!file_exists($font)) die("Font not found: $font");
