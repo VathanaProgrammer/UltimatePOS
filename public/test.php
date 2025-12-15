7799738919a96b5c6588bd5041a4e6e25ddc07dd
vathana <siengvathana1@gmail.com>
2025-12-15 10:54:47 +0700

    g

--- public/test.php
+++ public/test.php
@@ -1,3 +1,13 @@
 <?php
-$text = 'ខ្មែរ';
-var_dump(mb_detect_encoding($text, 'UTF-8', true));
+$img = imagecreatetruecolor(200, 80);
+$white = imagecolorallocate($img, 255, 255, 255);
+$black = imagecolorallocate($img, 0, 0, 0);
+imagefill($img, 0, 0, $white);
+
+$font = '/var/www/html/UltimatePOS/public/fonts/khmer/NotoSansKhmer-Regular.ttf';
+$text = 'សួស្តី'; // Some Khmer text
+
+imagettftext($img, 20, 0, 10, 50, $black, $font, $text);
+imagepng($img, 'test.png');
+imagedestroy($img);
+echo "Done";
