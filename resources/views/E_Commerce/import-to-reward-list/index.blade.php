1831c3559066d70962795d93abe05bf3bbbe7362
Seng Vengchhuong <leobomo5@gmail.com>
2025-12-24 10:58:47 +0700

    remove filters

--- resources/views/E_Commerce/import-to-reward-list/index.blade.php
+++ resources/views/E_Commerce/import-to-reward-list/index.blade.php
@@ -15,7 +15,7 @@
 
     <section class="content">
         <!-- Filters -->
-        @component('components.filters_custom', ['title' => __('Filters')])
+        <!-- @component('components.filters_custom', ['title' => __('Filters')])
             <div class="col-md-3">
                 {!! Form::label('search_text', __('Search') . ':') !!}
                 {!! Form::text('search_text', null, [
@@ -24,7 +24,7 @@
                     'placeholder' => 'Product name, SKU, category...',
                 ]) !!}
             </div>
-        @endcomponent
+        @endcomponent -->
 
         <div class="box">
             <div class="box-header with-border">
