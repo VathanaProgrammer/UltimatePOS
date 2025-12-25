5a50de2a99b2f0ad954a54c2d1b0d4bc94ad3d9b
Seng Vengchhuong <leobomo5@gmail.com>
2025-12-25 11:35:24 +0700

    fix

--- resources/views/E_Commerce/telegram/index.blade.php
+++ resources/views/E_Commerce/telegram/index.blade.php
@@ -2,25 +2,32 @@
 @section('title', __('Telegram Link'))
 
 @section('content')
-<section class="content-header">
-    <h1 class="tw-text-xl md:tw-text-3xl tw-font-bold">
+<section class="content-header tw-py-4">
+    <h1 class="tw-text-xl md:tw-text-3xl tw-font-bold tw-text-black">
         {{ __('Telegram Link') }}
     </h1>
 </section>
 
-<section class="shadow-md rounded bg-white p-4 mt-4">
+<section class="content">
     {!! Form::open(['route' => 'telegramLinkUpdate', 'method' => 'post']) !!}
-    <div class="form-group">
-        {!! Form::label('telegram_link', __('Telegram Link')) !!}
-        {!! Form::text('telegram_link', $telegramLink ?? null, [
-            'class' => 'form-control',
-            'placeholder' => 'https://t.me/your_channel'
-        ]) !!}
+
+    <div class="tw-max-w-lg tw-mx-auto tw-mt-6">
+        <div class="tw-bg-white tw-shadow-md tw-p-6 tw-rounded">
+            <div class="form-group">
+                {!! Form::label('telegram_link', __('Telegram Link'), ['class' => 'tw-font-medium']) !!}
+                {!! Form::text('telegram_link', $telegramLink ?? null, [
+                    'class' => 'form-control tw-mt-2',
+                    'placeholder' => 'https://t.me/your_channel'
+                ]) !!}
+            </div>
+
+            <button type="submit"
+                class="tw-mt-4 tw-px-4 tw-py-2 tw-bg-red-600 tw-text-white tw-rounded hover:tw-bg-red-700">
+                {{ __('Save Changes') }}
+            </button>
+        </div>
     </div>
 
-    <button type="submit" class="tw-dw-btn tw-dw-btn-error tw-text-white">
-        {{ __('Save Changes') }}
-    </button>
     {!! Form::close() !!}
 </section>
 @endsection
