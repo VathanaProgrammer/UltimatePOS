ad26abaf14da6ecd8ecccdb6ddb7e0def77d514e
Seng Vengchhuong <leobomo5@gmail.com>
2025-12-25 15:47:35 +0700

    fix

--- app/Http/Controllers/Api/TelegramController.php
+++ app/Http/Controllers/Api/TelegramController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use App\Models\BusinessLocation;
+use App\BusinessLocation;
 
 class TelegramController extends Controller
 {
