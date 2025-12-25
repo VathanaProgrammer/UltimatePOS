c0b44921c942414170453145cbbb2885266df4dd
Seng Vengchhuong <leobomo5@gmail.com>
2025-12-25 15:44:13 +0700

    add slash

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ use App\Http\Controllers\Api\TelegramController;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Crypt;
 
-Route::get('telegram-link', [TelegramController::class, 'getTelegramLink'])->name('api.telegram');
+Route::get('/telegram-link', [TelegramController::class, 'getTelegramLink'])->name('api.telegram');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
